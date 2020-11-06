540ff3cdabc8b38449f86fb51661d3c915a0683a
webdev778 <webdev778@gmail.com>
2020-11-06 11:47:22 +0800

    Enable frozen data

--- lib/iso-639-data.rb
+++ lib/iso-639-data.rb
@@ -5,16 +5,30 @@ module ISO_639_DATA
   class Error < StandardError; end
 
   ISO_639_2 = lambda do
-    YAML.load(File.read(File.join(File.dirname(__FILE__), '2.yaml')))
+    YAML.load(File.read(File.join(File.dirname(__FILE__), '2.yaml'))).tap do |h|
+      h.each do |k,v|
+        v.freeze
+      end
+    end
   end.call.freeze
 
   ISO_639_3 = lambda do
-    dataset = YAML.load(File.read(File.join(File.dirname(__FILE__), '3.yaml')))
+    YAML.load(File.read(File.join(File.dirname(__FILE__), '3.yaml'))).tap do |h|
+      h.each do |k,v|
+        v.freeze
+      end
+    end
   end.call.freeze
 
   class << self
     def codes
       (ISO_639_2.keys << ISO_639_3.keys).flatten.uniq
+    end
+    def iso_639_2
+      ISO_639_2
+    end
+    def iso_639_3
+      ISO_639_3
     end
   end
 end
--- spec/iso-639-data_spec.rb
+++ spec/iso-639-data_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe ISO_639_DATA do
   it "should be uniq" do
     expect(ISO_639_DATA.codes.length).to be ISO_639_DATA.codes.uniq.length
   end
+
+  it "should be immutable" do
+    expect { ISO_639_DATA.iso_639_2["aar"] = {"iso_639_1": "custom"} }.to raise_error(FrozenError)
+    expect { ISO_639_DATA.iso_639_2["aar"]["iso_639_1"] = "custom" }.to raise_error(FrozenError)
+    expect { ISO_639_DATA.iso_639_3["aaa"] = {"Part2B": "custom"} }.to raise_error(FrozenError)
+    expect { ISO_639_DATA.iso_639_3["aaa"]["Ref_Name"] = "custom" }.to raise_error(FrozenError)
+  end
 end
